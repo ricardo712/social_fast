2c3831ea7f5da45903e63ebcc5b5bf0dd8d25a41
JhonRuiz95 <jhonjairo.45@hotmail.com>
2022-04-22 09:58:02 -0500

    Notificaciones terminado

--- lib/screens/notificaciones.dart
+++ lib/screens/notificaciones.dart
@@ -8,6 +8,7 @@ import 'package:social_fast/models/user_model.dart';
 import 'package:social_fast/ui/widgets.dart';
 import 'package:social_fast/utils/responsive.dart';
 import 'package:social_fast/widgets/circle.dart';
+import 'package:social_fast/widgets/mod_notificaccion.dart';
 import 'package:social_fast/widgets/mod_publicacion.dart';
 
 class Notificaciones extends StatefulWidget {
@@ -41,7 +42,13 @@ class _NotificacionesState extends State<Notificaciones> {
     final double pinkSize = responsive.wp(90);
     final double orangeSize = responsive.wp(55);
     final size = MediaQuery.of(context).size;
+    final mensajeNotificacion =
+        "Tienes una nueva sugerencia\nde amistad de ${pubModel.uName}";
+    final fecha = pubModel.fecha;
+    final menPub = pubModel.mensaje;
     objeto(pubModel);
+    String sugerencia = "Tienes una nueva sugerencia\n de amistad";
+    String like = "Le gusta tu foto a ";
 
     return Scaffold(
       appBar: AppBar(
@@ -91,7 +98,82 @@ class _NotificacionesState extends State<Notificaciones> {
               ),
               ListView(
                 padding: EdgeInsets.all(responsive.dp(1.5)),
-                children: <Widget>[],
+                children: <Widget>[
+                  Container(
+                    //color: Colors.deepPurpleAccent,
+                    padding: EdgeInsets.all(responsive.dp(2)),
+                    width: responsive.wp(94.3),
+                    //height: responsive.hp(85),
+                    decoration: BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          responsive.dp(7),
+                        ),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Notificaciones.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: responsive.dp(3),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Nuevas.",
+                              //textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: responsive.dp(2),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: responsive.hp(1),
+                        ),
+                        modNotificacion(mensaje: sugerencia, name: "Jhon Ruiz"),
+                        SizedBox(
+                          height: responsive.hp(1),
+                        ),
+                        modNotificacion(
+                          mensaje: sugerencia,
+                          name: "Mateo Avila",
+                        ),
+                        SizedBox(
+                          height: responsive.hp(1),
+                        ),
+                        modNotificacion(mensaje: sugerencia, name: "Carmen St"),
+                        SizedBox(height: responsive.hp(1)),
+                        Row(
+                          children: [
+                            Text(
+                              "Anteriores.",
+                              //textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: responsive.dp(2),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: responsive.hp(1)),
+                        modNotificacion(mensaje: like, name: "Carmen St"),
+                        SizedBox(height: responsive.hp(1)),
+                        modNotificacion(mensaje: like, name: "Anastasia "),
+                        SizedBox(height: responsive.hp(1)),
+                        modNotificacion(mensaje: like, name: "Anastasia "),
+                        SizedBox(height: responsive.hp(1)),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
---
+++ lib/widgets/mod_notificaccion.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:social_fast/models/pub_model.dart';
+import 'package:social_fast/utils/responsive.dart';
+
+class modNotificacion extends StatelessWidget {
+  String name;
+  String mensaje;
+  modNotificacion({Key? key, required this.name, required this.mensaje})
+      : assert(name != "" || name != null),
+        assert(mensaje != "" || mensaje != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PubModel pubModel = PubModel();
+    Responsive responsive = Responsive(context);
+    objeto(pubModel);
+    final mensajeNotificacion = "${mensaje} ${name}";
+    final fecha = pubModel.fecha;
+    final menPub = pubModel.mensaje;
+
+    return Container(
+      //color: Colors.deepPurpleAccent,
+      padding: EdgeInsets.all(responsive.dp(2)),
+      width: responsive.wp(94.3),
+      height: responsive.hp(11.6),
+      decoration: BoxDecoration(
+        color: Colors.black38,
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            responsive.dp(7),
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              CircleAvatar(
+                radius: responsive.dp(3),
+                backgroundImage: AssetImage('assets/images/a.jpg'),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: responsive.wp(2),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    mensajeNotificacion,
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: responsive.dp(2),
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void objeto(PubModel pubModel) {
+    pubModel.id = '1';
+    pubModel.uid = '1058';
+    pubModel.uName = 'Jhon Ruiz';
+    pubModel.mensaje = 'Un buen paisaje.';
+    pubModel.fecha = '07/04/2022';
+    pubModel.hora = '16:36';
+    pubModel.imagen = 'vacio';
+  }
+}
